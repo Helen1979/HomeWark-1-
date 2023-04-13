160d4ce826e0a6a9d7c81316f3d1759c2b10c5d1
Helen1979 <124290248+Helen1979@users.noreply.github.com>
2023-04-13 20:18:37 +0300

    Update HW-1/Task1.py
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- HW-1/Task1.py
+++ HW-1/Task1.py
@@ -32,7 +32,7 @@
 # s = str(input('Введите шесть чисел вашего билета: '))
 # sum1 = int(s[0]) + int(s[1]) + int(s[2])
 # sum2 = int(s[3]) + int(s[4]) +int(s[5])
-# if sum1==sum2:
+# if sum1 == sum2:
 #   print('Ваш билет счастливый')
 # else:
 #   print('Ваш билет обычный')
